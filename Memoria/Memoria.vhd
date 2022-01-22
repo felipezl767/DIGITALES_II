59bd3ad3c42c01fc6f9b66cebc6cfaa3e2adc2ca
felipezl767-new <felipezl767@gmail.com>
2022-01-22 10:51:45 -0500

    Update Memoria.vhd

--- Memoria/Memoria.vhd
+++ Memoria/Memoria.vhd
@@ -59,6 +59,8 @@ architecture memoriaarch of Memoria is
 											port_out_02,port_out_03,port_out_04,port_out_05,port_out_06,
 											port_out_07,port_out_08,port_out_09,port_out_10,port_out_11,
 											port_out_12,port_out_13,port_out_14,port_out_15);
+   sevenSeg : sevensegment port map (sw,pb,hex0);												
+										
 											
 	MUX1 : process (address, rom_data_out, rw_data_out, port_in_00, port_in_01, port_in_02,
 							port_in_03, port_in_04, port_in_05, port_in_06, port_in_07, port_in_08,

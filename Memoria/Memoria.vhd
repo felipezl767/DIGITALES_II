a8e5c3eaa24839ecf1b792980d310c5d1abd8960
felipezl767-new <felipezl767@gmail.com>
2022-01-22 10:47:59 -0500

    add 7seg

--- Memoria/Memoria.vhd
+++ Memoria/Memoria.vhd
@@ -9,6 +9,9 @@ entity Memoria is
 	
 	port(clock, reset, writen		: in std_logic	;
 			address, data_in 			: in std_logic_vector(7 downto 0);
+			sw: in STD_LOGIC_VECTOR(2 downto 0);
+			pb: in STD_LOGIC;
+			hex0: out STD_LOGIC_VECTOR(0 to 7);
 			port_in_00, port_in_01, port_in_02, port_in_03, port_in_04, port_in_05,
 			port_in_06, port_in_07, port_in_08, port_in_09, port_in_10, port_in_11,
 			port_in_12, port_in_13, port_in_14, port_in_15 	: in std_logic_vector(7 downto 0);
@@ -34,10 +37,16 @@ architecture memoriaarch of Memoria is
 	
 	component Entradas 
 		port(clock, reset, writen 	: in std_logic;
-		address, data_in : in std_logic_vector(7 downto 0);
-		port_out_00, port_out_01,port_out_02,port_out_03,port_out_04,port_out_05,port_out_06,
-		port_out_07,port_out_08,port_out_09,port_out_10,port_out_11,port_out_12,port_out_13,
-		port_out_14,port_out_15		: out std_logic_vector(7 downto 0));
+				address, data_in : in std_logic_vector(7 downto 0);
+				port_out_00, port_out_01,port_out_02,port_out_03,port_out_04,port_out_05,port_out_06,
+				port_out_07,port_out_08,port_out_09,port_out_10,port_out_11,port_out_12,port_out_13,
+				port_out_14,port_out_15		: out std_logic_vector(7 downto 0));
+	end component;
+	
+	component sevensegment
+		port(sw: IN STD_LOGIC_VECTOR(2 downto 0);
+				pb: IN STD_LOGIC;
+				hex0: OUT STD_LOGIC_VECTOR(0 to 7));	
 	end component;
 	
 	signal rom_data_out, rw_data_out,data_out 	: std_logic_vector(7 downto 0);
